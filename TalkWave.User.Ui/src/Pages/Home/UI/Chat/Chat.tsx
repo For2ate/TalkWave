4378e6ce5d1adc1c8e31feef85553c7e48ab46a6
Ivan_Skrypinski <ivanskrypinski@gmail.com>
2025-03-20 15:11:56 +0300

    some fix

--- TalkWave.User.Ui/src/Pages/Home/UI/Chat/Chat.tsx
+++ TalkWave.User.Ui/src/Pages/Home/UI/Chat/Chat.tsx
@@ -3,7 +3,7 @@ import styles from "./Chat.module.css";
 export const Chat = () => {
   return (
     <div className={styles.layout}>
-      <h1>NIgger</h1>
+      <h1>chat</h1>
     </div>
   );
 };
--- TalkWave.User.Ui/src/Pages/Home/UI/Sidebar/Sidebar.tsx
+++ TalkWave.User.Ui/src/Pages/Home/UI/Sidebar/Sidebar.tsx
@@ -1,4 +1,10 @@
-import { IconButton, OpenSidebarIcon, CloseSidebarIcon } from "Shared/Ui";
+import {
+  IconButton,
+  OpenSidebarIcon,
+  CloseSidebarIcon,
+  StyledInput,
+  SearchIcon,
+} from "Shared/Ui";
 import { useSidebarContext } from "../../Contexts";
 
 import styles from "./Sidebar.module.css";
@@ -19,12 +25,20 @@ const OpenSidebar = () => {
   const { toggleSideBar } = useSidebarContext();
   return (
     <div className={openStyles.openLayout}>
-      <div className={openStyles.openHeader}>
-        <h1>TalkWave</h1>
-        <IconButton onClick={toggleSideBar} size={50}>
-          <CloseSidebarIcon color="white" />
-        </IconButton>
-      </div>
+      <header className={openStyles.header}>
+        <div className={openStyles.topHeader}>
+          <h1>TalkWave</h1>
+          <IconButton onClick={toggleSideBar} size={50}>
+            <CloseSidebarIcon color="white" />
+          </IconButton>
+        </div>
+        <div className={openStyles.searchBox}>
+          <StyledInput label="" onChange={() => {}} value="search" />
+          <IconButton size={35}>
+            <SearchIcon color={"white"} />
+          </IconButton>
+        </div>
+      </header>
     </div>
   );
 };
